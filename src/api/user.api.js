ed3af7940faaccdc06ab81df33f240f37b0d5491
Aamer444 <mohammadaamer788@gmail.com>
2024-03-25 00:39:56 +0530

    project setup

--- src/api/user.api.js
+++ src/api/user.api.js
@@ -2,6 +2,6 @@ import express from 'express'
 import * as userService from '../services/controller/user.controller.js'
 const router=express.Router()
 
-router.get('/user_details',userService.get_user_details)
+router.get('/user_details',userService.get_user_details)//
 
 export default router
